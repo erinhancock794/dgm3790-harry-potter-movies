0f36bc9597c36464beb460276c676efb5ecc649a
Erin Hancock <erinhancock794@gmail.com>
2021-09-28 19:01:39 -0600

    adding info and favorite icons

--- src/components/HarryPotterContainer.js
+++ src/components/HarryPotterContainer.js
@@ -1,34 +1,113 @@
+import * as React from "react";
 import { movieMetadata } from "../data/movieMetadata";
-import Movie from "./HarryPotterCard";
-import { Box } from "@material-ui/core";
+import HarryPotterCard from "./HarryPotterCard";
+import { Box, Modal, CardContent, Typography } from "@mui/material";
+
+const style = {
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: "400",
+  bgcolor: "background.paper",
+  border: "2px solid #000",
+  boxShadow: "24",
+  p: "4",
+};
+
 const HarryPotterContainer = () => {
+  const [open, setOpen] = React.useState(false);
+  const [loadData, setLoadData] = React.useState([]);
+  const [favorites, setFavorites] = React.useState([]);
+  const handleClose = () => {
+    return setOpen(false);
+  };
+  const loadMetadata = (movie) => {
+    setOpen(true);
+    setLoadData(() => {
+      return [movie];
+    });
+  };
+
+  const addToFavorites = (movie) => {
+    const isFavorite = Boolean(favorites.find((item) => item.imdbID === movie.imdbID));
+    if (!isFavorite) {
+      setFavorites((prevState) => [...prevState, movie]);
+    } else {
+      setFavorites(() => {
+        return favorites.filter((item) => item.imdbID !== movie.imdbID);
+      });
+    }
+  };
+
   return (
-      <div>
+    <div>
+    <Box
+    sx={{
+      display: "flex",
+      flexDirection: "column",
+      color: "#eee",
+      backgroundColor: "#00F",
+      justifyContent: "center",
+      p: 2
+    }}
+  >
+    <h1>Favorites</h1>
+    {favorites &&
+      favorites.map((movie) => {
+        return (
+          <p key={movie.imdbID} sx={{ color: "#fff" }}>
+            {movie.Title}
+          </p>
+        );
+      })}
+  </Box>
     <Box
       sx={{
         display: "flex",
         justifyContent: "center",
         flexWrap: "wrap",
-        width: '100%'
+        width: "100%",
       }}
     >
-        {movieMetadata.map((movie) => {
-          return (
-            <Movie
-              movieTitle={movie.Title}
-              year={movie.Year}
-              id={movie.imdbID}
-              poster={movie.Poster}
-              key={movie.imdbID}
-              imdbRating={movie.imdbRating}
-              metascore={movie.Metascore}
-              actors={movie.Actors}
-              runtime={movie.Runtime}
-              director={movie.Director}
-              plot={movie.Plot}
-            />
-          );
-        })}
+
+      {movieMetadata.map((movie) => {
+        return (
+          <HarryPotterCard
+            key={movie.imdbID}
+            loadMetadataFunction={loadMetadata}
+            addToFavoritesFunction={addToFavorites}
+            movie={{ ...movie }}
+          />
+        );
+      })}
+      <Modal open={open} onClose={handleClose}>
+        <Box sx={style}>
+          <CardContent
+            sx={{
+              bgcolor: "#00F",
+              color: "#EEE"
+            }}
+          >
+            {loadData.map((movie) => {
+              return (
+                <div>
+                  <Typography component="h1">{movie.Title}</Typography>
+                  <Typography>{movie.Plot}</Typography>
+                  <Typography>{`Release Date: ${movie.Year}`}</Typography>
+                  <Typography>{`Runtime: ${movie.Runtime}`}</Typography>
+                  <Typography>{`Director: ${movie.Director}`}</Typography>
+                  <Typography>{`Cast: ${movie.Actors}`}</Typography>
+                  <Typography>
+                    {`Rotten Tomatoes: ${movie.Metascore}`}
+                  </Typography>
+                  <Typography>{`imdb Rating: ${movie.imdbRating}`}</Typography>
+                </div>
+              );
+            })}
+          </CardContent>
+        </Box>
+      </Modal>
     </Box>
     </div>
   );
